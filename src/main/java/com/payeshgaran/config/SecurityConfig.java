cea97cfbee8c2079ff180ccb9e1016f4eeaf1774
eadine <erfan.adine.payeshgaranmodiriat@gmil.com>
2022-08-24 14:59:47 +0430

    first commit 2

--- src/main/java/com/payeshgaran/config/SecurityConfig.java
+++ src/main/java/com/payeshgaran/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+
+
     }
 
     @Override
